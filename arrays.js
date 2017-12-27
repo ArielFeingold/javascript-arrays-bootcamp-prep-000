de3c3a8a59998f66c971c841748641a1f97e29ab
Ariel Feingold <feingold.ariel@gmail.com>
2017-12-27 16:42:11 +0000

    automatically backed up by learn

--- arrays.js
+++ arrays.js
@@ -5,6 +5,6 @@ var chocolateBars = ["snickers", "hundred grand", "kitkat", "skittles"];
 function addElementToBeginningOfArray(array, element){
 var arr = [array]
 var arr2 = [element, ...array]
-return arr
+return arr2
 }
 
