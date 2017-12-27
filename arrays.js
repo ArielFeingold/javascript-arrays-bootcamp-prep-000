7373419664239600a9baf21694e18928e39ce393
Ariel Feingold <feingold.ariel@gmail.com>
2017-12-27 16:26:39 +0000

    automatically backed up by learn

--- arrays.js
+++ arrays.js
@@ -3,8 +3,8 @@ var chocolateBars = ["snickers", "hundred grand", "kitkat", "skittles"];
 
 
 function addElementToBeginningOfArray(array, element){
-var array = new Array();
-var array2 = [element, ...array];
+const array = [];
+const array2 = [element, ...array];
 return array2;
 }
 
