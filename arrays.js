b24cb00b8bf7c9b90a570a2f98b95d30c76ed615
Ariel Feingold <feingold.ariel@gmail.com>
2017-12-27 15:46:17 +0000

    automatically backed up by learn

--- arrays.js
+++ arrays.js
@@ -5,11 +5,5 @@ var chocolateBars = ["snickers", "hundred grand", "kitkat", "skittles"];
 function addElementToBeginningOfArray(array, element){
  var array = ["foo"];
  array.unshift(element);
-return array
+return array;
 }; 
-
-// function destructivelyAddElementToBeginningOfArray(x){
-//   const arr = ["foo"];
-//   arr.unshift();
-//   return arr
-// } 
